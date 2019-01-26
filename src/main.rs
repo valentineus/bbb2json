b591a9c5676f50d226357f4ced531c9f3c62fe08
Valentin Popov <info@valentineus.link>
2019-01-26 20:19:06 +0400

    Sat, 26 Jan 2019 20:19:06 +0400
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::fs::File;
 use std::io::BufReader;
 
+extern crate clap;
+use clap::{load_yaml, App};
+
 mod parser;
 use parser::*;
 
 fn main() {
-	let file: File = File::open("events.xml").unwrap();
+	let yaml = load_yaml!("cli/ru.yml");
+	let matches = App::from_yaml(yaml).get_matches();
+
+	let path = matches.value_of("FILE").unwrap();
+
+	let file: File = File::open(path).unwrap();
 	let data: ParserResult = parse(BufReader::new(file));
 	dbg!(data.meeting_name);
 }
